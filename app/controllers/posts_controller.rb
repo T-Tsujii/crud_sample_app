3d98f7669bcbef7dcf9386bfeb047995ae8b6529
T.Tsujii <ttakehisabt@gmail.com>
2020-06-06 22:10:15 +0900

    ヘッダーを部分テンプレート化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.order(id: :asc)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,13 +18,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to post
+    @post.update!(post_params)
+    redirect_to @post
   end
 
   def destroy
@@ -33,6 +32,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
